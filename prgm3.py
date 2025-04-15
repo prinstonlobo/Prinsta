f29d2a0a1dbeb7788feec13fbef50765483afeea
prinstonlobo <prinstonlobo1127@gmail.com>
2025-04-15 14:31:25 +0530

    Add files via upload

---
+++ prgm3.py
@@ -0,0 +1,10 @@
+class A:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def display(self):
+        print(f"MY name is {self.name} and i am {self.age} years old")
+o=A("printon",23)
+o.display()
+print(getattr(o,"salary","NF"))
+print(hasattr(o,"age"))
